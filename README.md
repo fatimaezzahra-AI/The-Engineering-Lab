703b83ba21f38ff06b680bdfc8e0beae013bc0b2
fatimaezzahra-AI <fatimaezzahraboukhorssa@gmail.com>
2026-01-15 20:55:26 +0000

    First build of my Lab

---
+++ README.md
@@ -0,0 +1,2 @@
+# The Engineering Lab
+My projects hub for AI and Engineering.
---
+++ app.py
@@ -0,0 +1,3 @@
+import streamlit as st
+st.title("The Engineering Lab 🚀")
+st.write("Hada howa l-Portfolio dial Fatima-Ezzahra!")
