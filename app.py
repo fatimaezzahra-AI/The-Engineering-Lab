7229eb231fd24366de4864f6c101fb7354ff9f8e
fatimaezzahra-AI <boukhorssaf82@gmail.com>
2026-08-10 10:21:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -61,13 +61,13 @@ if selected == "About":
         """)
         
         st.markdown("####  My Core Skills:")
-        st.success(" **Machine Learning & Predictive Modeling:**Scikit-Learn (Random Forest, XGBoost), statistical modeling, feature engineering.")
-        st.success(" **Python, Pandas, Plotly, Streamlit — building interactive dashboards from raw data to insights.") 
-        st.success(" **Embedded Systems & Signal Processing:** Arduino, biomedical sensors, real-time data acquisition.")
+        st.success(" Machine Learning & Predictive Modeling:**Scikit-Learn (Random Forest, XGBoost), statistical modeling, feature engineering.")
+        st.success(" Python, Pandas, Plotly, Streamlit — building interactive dashboards from raw data to insights.") 
+        st.success(" Embedded Systems & Signal Processing:** Arduino, biomedical sensors, real-time data acquisition.")
         
     with col2:
 
-        st.image("images/maphoto.png", width=250)
+        st.image("assets/maphoto.png", width=250)
 
 elif selected == "Projects":
     st.markdown("###  Technical Project Portfolio")
